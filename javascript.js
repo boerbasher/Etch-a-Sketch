61bc29ab30f7deb23b117273f707b581014c1ae4
boerbasher <boerbasher@protonmail.com>
2022-07-13 22:15:03 +0100

    Use crosshair cursor hovering squares

--- javascript.js
+++ javascript.js
@@ -1,11 +1,14 @@
 let square = document.querySelector('div');
-let coloured = '#edf6d6'
+let coloured = '#C3DCE5'
+const element = document.getElementById('starter-button');
+
 
 function makeRow() {
-    for (let i = 0; i < 16; i++) {
+    element.remove()
+    for (let i = 0; i < 32; i++) {
         const row = document.createElement('div');
         row.className = 'row';
-        for (let x = 0; x < 16; x++) {
+        for (let x = 0; x < 32; x++) {
             let square = document.createElement('div');
             square.className = "square";
             square.setAttribute("id", "square")
