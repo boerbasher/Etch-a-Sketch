27b21cd870915aff5196563ffb0b0761bcbb8f69
boerbasher <boerbasher@protonmail.com>
2022-07-13 21:45:47 +0100

    ...

--- javascript.js
+++ javascript.js
@@ -2,22 +2,16 @@ let square = document.querySelector('div');
 let coloured = '#edf6d6'
 
 function makeRow() {
-    const resolution = prompt()
-    for (let i = 0; i < parseInt(resolution); i++) {
+    for (let i = 0; i < 16; i++) {
         const row = document.createElement('div');
         row.className = 'row';
-        for (let x = 0; x < parseInt(resolution); x++) {
+        for (let x = 0; x < 16; x++) {
             let square = document.createElement('div');
             square.className = "square";
             square.setAttribute("id", "square")
             row.appendChild(square);
         }
         document.getElementById('grid').appendChild(row);
-    }
-    if (resolution == 8) {
-        square.setAttribute('');    
-    } else if (resolution == 16) {
-        square.setAttribute('style', 'background: red; width: 24px; height: 24px')
     }
 }
 
