651f18b5fd278ae41acc7bd32ec5b85a2e7a9449
boerbasher <boerbasher@protonmail.com>
2022-07-15 15:28:40 +0100

    Add Eraser Functionality

--- javascript.js
+++ javascript.js
@@ -1,10 +1,26 @@
 let square = document.querySelector('div');
-let coloured = '#C3DCE5'
-const element = document.getElementById('starter-button');
+let mainArea = document.getElementById('container')
+const element = document.getElementsByClassName('h');
+
+let height = '100px';
+let width = '100px';
+/*
+var pad2 = "calc(" + width + " - 90px)";
+*/
 
 
+function removeElementsByClass(className){
+    const elements = document.getElementsByClassName('but');
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
 function makeRow() {
-    element.remove()
+    let conty = document.getElementById('container');
+    let griddy = document.createElement('div');
+    conty.appendChild(griddy)
+    griddy.setAttribute("id", "grid")
+    removeElementsByClass() 
     for (let i = 0; i < 32; i++) {
         const row = document.createElement('div');
         row.className = 'row';
@@ -12,12 +28,22 @@ function makeRow() {
             let square = document.createElement('div');
             square.className = "square";
             square.setAttribute("id", "square")
+            square.style.backgroundColor = 'rgb(17, 12, 34)';
             row.appendChild(square);
         }
         document.getElementById('grid').appendChild(row);
+        let pixelHeight = "calc(" + height + " - 90px)";
+        /*square.style.height = pixelHeight;*/
     }
 }
 
-square.addEventListener('click', function (e) {
-    e.target.style.background = coloured;
+square.addEventListener('mousedown', function (e) {
+    if(e.target.id === 'square'){
+        if (e.target.style.backgroundColor === 'rgb(17, 12, 34)'){
+            e.target.style.backgroundColor = 'rgb(195, 220, 229)';
+        } else {
+            e.target.style.backgroundColor = 'rgb(17, 12, 34)';
+        }
+    }
+        
   });
